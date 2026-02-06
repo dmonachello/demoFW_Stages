41be7d5f8cf11a3b61d6f7fe3653f67b122b4024
dmonache <dmonachello@verizon.net>
2026-02-05 20:01:53 -0500

    adjust PIDF constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,7 +16,7 @@ public final class Constants {
 
   public static final class Shooter {
     public static final int kLeaderId = 25;
-    public static final int kFollowerId = 40;
+    public static final int kFollowerId = 22;
     public static final boolean kFollowerInverted = true;
 
     // RPM setpoints for the demo.
@@ -26,10 +26,10 @@ public final class Constants {
     public static final double kMaxRpm = 5700.0;
 
     // Basic velocity PID + feedforward gains for a NEO on a Spark Max.
-    public static final double kP = 0.00018;
+    public static final double kP = 0.00030; //  was 0.00018 (0.00045 oscillated a lot)
     public static final double kI = 0.0;
     public static final double kD = 0.00003;
-    public static final double kFF = 0.961 / kMaxRpm;
+    public static final double kFF = 0.961 / kMaxRpm; // 
     public static final double kIZone = 0.0;
     public static final double kAllowedClosedLoopError = 75.0;
     public static final int kSmartCurrentLimit = 40;
